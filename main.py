c23d647eb4f09c8b23ce563ad8e103287627ce30
nurgazy <megalifekg@gmail.com>
2026-04-16 18:00:04 +0600

    refactoring

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from fastapi import FastAPI
+import os
+
+from fastapi import FastAPI, UploadFile, File
 
 app = FastAPI()
 
+UPLOAD_DIR = "temp_files"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World my friend Nurgazy"}
+    return {"Hello": "Hello my friend"}
+
+@app.post("/upload-1c")
+async def upload_from_1c(file: UploadFile = File(...)):
+    file_id = f"{file.filename}"
+    file_path = os.path.join(UPLOAD_DIR, file_id)
+
+    with open(file_path, "wb") as buffer:
+        content = await file.read()
+        buffer.write(content)
+
+    return {"status": "saved", "file_id": file_id}
